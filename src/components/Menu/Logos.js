f4513464525f8b17bf2e14e901c474eb4b5ce8cc
cemoret <cemoret.com>
2019-11-21 12:03:52 +0100

    landing

--- src/components/Menu/Logos.js
+++ src/components/Menu/Logos.js
@@ -13,7 +13,7 @@ class Logos extends Component {
     return (
       <div className="">
         <div className="logo-header-left">
-          <Link to={`/`}>
+          <a href="https://www.instagram.com/cafeteriaindustrial/" target="_blank">
             <img
               src={
                 'https://firebasestorage.googleapis.com/v0/b/cafeteria-industrial.appspot.com/o/images%2FcafeteriaIndustrail-logo.png?alt=media&token=fe86b56b-9d8b-4996-bda9-830645037078'
@@ -21,7 +21,16 @@ class Logos extends Component {
               className={`isotype`}
               alt="Cafeteria Industrial Isotype"
             />
-          </Link>
+          </a>
+          {/* <Link to={`/`}>
+            <img
+              src={
+                'https://firebasestorage.googleapis.com/v0/b/cafeteria-industrial.appspot.com/o/images%2FcafeteriaIndustrail-logo.png?alt=media&token=fe86b56b-9d8b-4996-bda9-830645037078'
+              }
+              className={`isotype`}
+              alt="Cafeteria Industrial Isotype"
+            />
+          </Link> */}
         </div>
       </div>
     );
