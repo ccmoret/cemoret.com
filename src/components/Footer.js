05e99c6693f14e6ca3f58465b8ca124bb965ffc2
cemoret <cemoret.com>
2020-01-12 20:53:08 +0100

    shop test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,30 +13,26 @@ class Footer extends Component {
     return (
       <footer>
         <div className="row py-2">
-          <div className="col-3">
-            <div className="pl-5">
-              <Link to={`/`}>
-                <h4>Process</h4>
-              </Link>
-              <Link to={`/`}>
-                <h4>About</h4>
-              </Link>
-            </div>
+          <div className="col-md-3 col-12 text-center-md pl-5">
+            <Link to={`/`}>
+              <h4>Process</h4>
+            </Link>
+            <Link to={`/`}>
+              <h4>About</h4>
+            </Link>
           </div>
-          <div className="col-6 text-center">
+          <div className="col-md-6 col-12 text-center">
             <h4 className="bold">CAFETERIA INDUSTRIAL</h4>
             <h4>Carrer de Pallars, 154, 08005 Barcelona / Telf: 930 27 71 49</h4>
           </div>
-          <div className="col-3 text-right">
-            <div className="pr-5">
-              <h4>Eng/Esp/Cat</h4>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.instagram.com/cafeteriaindustrial">
-                <h4>Instagram</h4>
-              </a>
-            </div>
+          <div className="col-md-3 col-12 text-right text-center-md pr-5">
+            <h4>Eng/Esp/Cat</h4>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/cafeteriaindustrial">
+              <h4>Instagram</h4>
+            </a>
           </div>
         </div>
       </footer>
